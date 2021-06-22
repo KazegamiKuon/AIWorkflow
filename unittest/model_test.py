10c1035a44ea955e9c922271007e1f1ee52d55c6
Kazegami <cuongquocdo1998@gmail.com>
2021-06-22 13:49:32 +0700

    test

--- unittest/model_test.py
+++ unittest/model_test.py
@@ -2,7 +2,7 @@ import unittest
 import torch
 from torch import nn
 import torchvision.models as models
-from lib.model.resnet import ResNetBasicBlock, ResNetBottleNeckBlock, ResNetLayer, resnet18
+# from lib.model.resnet import ResNetBasicBlock, ResNetBottleNeckBlock, ResNetLayer, resnet18
 from torchsummary import summary
 
 class TestAutoLayer(unittest.TestCase):
@@ -10,30 +10,30 @@ class TestAutoLayer(unittest.TestCase):
         super().__init__(methodName=methodName)
         pass
 
-    def test_resnet_basic_block(self):
-        dummy = torch.ones((1, 32, 224, 224))
-        block = ResNetBasicBlock(32, 64)
-        print(block(dummy).shape)
-        print(block)
-        pass
+    # def test_resnet_basic_block_test(self):
+    #     dummy = torch.ones((1, 32, 224, 224))
+    #     block = ResNetBasicBlock(32, 64)
+    #     print(block(dummy).shape)
+    #     print(block)
+    #     pass
     
-    def test_resnet_bottleneck(self):
-        dummy = torch.ones((1, 32, 10, 10))
-        block = ResNetBottleNeckBlock(32, 64)
-        print(block(dummy).shape)
-        print(block)
-        pass
+    # def test_resnet_bottleneck(self):
+    #     dummy = torch.ones((1, 32, 10, 10))
+    #     block = ResNetBottleNeckBlock(32, 64)
+    #     print(block(dummy).shape)
+    #     print(block)
+    #     pass
 
-    def test_resnet_layer(self):
-        dummy = torch.ones((1, 64, 48, 48))
-        layer = ResNetLayer(64, 128, block=ResNetBasicBlock, n=3)
-        print(layer(dummy).shape)
-        pass
+    # def test_resnet_layer(self):
+    #     dummy = torch.ones((1, 64, 48, 48))
+    #     layer = ResNetLayer(64, 128, block=ResNetBasicBlock, n=3)
+    #     print(layer(dummy).shape)
+    #     pass
     
-    def test_resnet18(self):
-        model = resnet18(3, 1000)
-        summary(model.cuda(), (3, 224, 224))
-        pass
+    # def test_resnet18(self):
+    #     model = resnet18(3, 1000)
+    #     summary(model.cuda(), (3, 224, 224))
+    #     pass
     
     def test_resnet18_data(self):
         summary(models.resnet18(False).cuda(), (3, 224, 224))
